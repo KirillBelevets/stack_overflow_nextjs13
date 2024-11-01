b730cd3e7dc00132921d1b1524f9224d71581fd1
Kirill Belevets <kirillbelevets@Kirills-MacBook-Air.local>
2024-11-01 09:54:04 +0200

    add ask question

---
+++ lib/utils.ts
@@ -0,0 +1,48 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const getTimestamp = (createdAt: Date): string => {
+  const createdDate =
+    typeof createdAt === "string" ? new Date(createdAt) : createdAt;
+
+  const now = new Date();
+  const elapsed = now.getTime() - createdAt.getTime();
+
+  const seconds = Math.floor(elapsed / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 24) {
+    return `${hours} hours ago`;
+  } else if (days < 7) {
+    return `${days} days ago`;
+  } else if (weeks < 5) {
+    return `${weeks} weeks ago`;
+  } else if (months < 12) {
+    return `${months} months ago`;
+  } else {
+    return `${years} years ago`;
+  }
+};
+
+export const formatLargeNumber = (num: number): string => {
+  if (num >= 1000000) {
+    return `${(num / 1000000).toFixed(1)}M`;
+  } else if (num >= 1000) {
+    return `${(num / 1000).toFixed(1)}K`;
+  } else {
+    return num.toString();
+  }
+};
